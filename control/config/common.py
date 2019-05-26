bf629693466cc37fa4a15b2b6b8d22e388cdf55f
Mathu Mounasamy <mathu.mounasamy@mediushealth.org>
2019-05-26 19:20:13 +1000

    Converts standard DRF (Django Rest Framework) APIs to JSON API APIs

--- control/config/common.py
+++ control/config/common.py
@@ -183,20 +183,54 @@ class Common(Configuration):
     # Custom user app
     AUTH_USER_MODEL = 'users.User'
 
-    # Django Rest Framework
+    # django rest framework json api specific config
     REST_FRAMEWORK = {
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
-        'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
-        'DEFAULT_RENDERER_CLASSES': (
-            'rest_framework.renderers.JSONRenderer',
-            'rest_framework.renderers.BrowsableAPIRenderer',
+        'PAGE_SIZE': 10,
+        'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+        'DEFAULT_PAGINATION_CLASS':
+            'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
+        'DEFAULT_PARSER_CLASSES': (
+            'rest_framework_json_api.parsers.JSONParser',
+            'rest_framework.parsers.FormParser',
+            'rest_framework.parsers.MultiPartParser'
         ),
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
-        'DEFAULT_AUTHENTICATION_CLASSES': (
-            'rest_framework.authentication.SessionAuthentication',
-            'rest_framework.authentication.TokenAuthentication',
-        )
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework_json_api.renderers.JSONRenderer',
+            # If you're performance testing, you will want to use the browseable API
+            # without forms, as the forms can generate their own queries.
+            # If performance testing, enable:
+            # 'example.utils.BrowsableAPIRendererWithoutForms',
+            # Otherwise, to play around with the browseable API, enable:
+            'rest_framework.renderers.BrowsableAPIRenderer'
+        ),
+        'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+        'DEFAULT_FILTER_BACKENDS': (
+            'rest_framework_json_api.filters.QueryParameterValidationFilter',
+            'rest_framework_json_api.filters.OrderingFilter',
+            'rest_framework_json_api.django_filters.DjangoFilterBackend',
+            'rest_framework.filters.SearchFilter',
+        ),
+        'SEARCH_PARAM': 'filter[search]',
+        'TEST_REQUEST_RENDERER_CLASSES': (
+            'rest_framework_json_api.renderers.JSONRenderer',
+        ),
+        'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
     }
+
+    # Django Rest Framework
+    # REST_FRAMEWORK = {
+    #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #     'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
+    #     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
+    #     'DEFAULT_RENDERER_CLASSES': (
+    #         'rest_framework.renderers.JSONRenderer',
+    #         'rest_framework.renderers.BrowsableAPIRenderer',
+    #     ),
+    #     'DEFAULT_PERMISSION_CLASSES': [
+    #         'rest_framework.permissions.IsAuthenticated',
+    #     ],
+    #     'DEFAULT_AUTHENTICATION_CLASSES': (
+    #         'rest_framework.authentication.SessionAuthentication',
+    #         'rest_framework.authentication.TokenAuthentication',
+    #     )
+
--- control/pulse/serializers.py
+++ control/pulse/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import serializers
+from rest_framework import serializers as drf_serializers
+from rest_framework_json_api import serializers
 from .models import Pulse
 
 
