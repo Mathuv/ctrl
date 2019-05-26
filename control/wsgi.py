1ec6412d797c66b0076df53585480cfc3f0e2883
Mathu Mounasamy <mathu.mounasamy@mediushealth.org>
2019-05-26 23:49:49 +1000

    Adds standard settings files instead os config with django-configurations

--- control/wsgi.py
+++ control/wsgi.py
@@ -6,8 +6,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control.config")
+# os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 
 from configurations.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
